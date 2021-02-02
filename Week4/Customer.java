f58a0e698448cfe40ad4d1ece52c3961eb64e624
Michael Miljanovic <michael.miljanovic@uoit.ca>
2021-02-02 12:41:17 -0500

    Fixed an error

--- Week4/Customer.java
+++ Week4/Customer.java
@@ -37,7 +37,7 @@ public String statement() {
 		// add frequent renter points
 		frequentRenterPoints ++;
 		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 			 frequentRenterPoints ++;
+		if ((each.getMovie().getPriceCode() == 1) && each.getDaysRented() > 1) 			 frequentRenterPoints ++;
 		//show figures for this rental
 		result += "\t" + each.getMovie().getTitle()+ "\t" +
 		String.valueOf(thisAmount) + "\n";
