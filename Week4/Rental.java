30758b39cabed12338f82750b8d3b9af90b30883
Michael Miljanovic <michael.miljanovic@uoit.ca>
2021-02-02 10:15:10 -0500

    Week 3 sols and Week 4 code

---
+++ Week4/Rental.java
@@ -0,0 +1,14 @@
+class Rental {
+public Movie _movie;
+public int _daysRented;
+public Rental(Movie movie, int daysRented) {
+_movie = movie;
+_daysRented = daysRented;
+}
+public int getDaysRented() {
+return _daysRented;
+}
+public Movie getMovie() {
+return _movie;
+}
+}
